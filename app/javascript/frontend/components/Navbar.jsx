fc3fce357a535d907f7c0b37d9afaee8748d76a5
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-12-04 08:55:27 +0200

    Fix logout btn error

--- app/javascript/frontend/components/Navbar.jsx
+++ app/javascript/frontend/components/Navbar.jsx
@@ -1,28 +1,22 @@
 import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { AppContext } from "./AppContext";
 export default function Navbar() {
   const { loggedInStatus, handleSuccessfulLogOut } = useContext(AppContext);
-  const navigate = useNavigate();
-  const signInRouter = navigate("/");
 
   const handleLogOut = () => {
-    if (loggedInStatus === "LOGGED_IN") {
-      fetch("http://localhost:3000/logout", {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-        mode: "cors",
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.logged_out) {
-            handleSuccessfulLogOut();
-          }
-        });
-    } else {
-      signInRouter();
-    }
+    fetch("http://localhost:3000/logout", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.logged_out) {
+          handleSuccessfulLogOut();
+        }
+      });
   };
   return (
     <nav>
