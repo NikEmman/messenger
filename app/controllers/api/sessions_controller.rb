5dcf0830315e22a80b93346d32c7cb51b6197c66
NikEmman <121699424+NikEmman@users.noreply.github.com>
2025-03-28 19:11:46 +0200

    Allow cors access to seperate react app

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,50 +1,50 @@
 module Api
-class SessionsController < ApplicationController
-  include CurrentUserConcern
+  class SessionsController < ApplicationController
+    include CurrentUserConcern
 
-  def create
-    user = User.find_by(email: session_params[:email])&.authenticate(session_params[:password])
+    def create
+      user = User.find_by(email: session_params[:email])&.authenticate(session_params[:password])
 
-    if user
-      session[:user_id] = user.id
-      render json: {
-        status: :created,
-        logged_in: true,
-        user: user
-      }, status: :created
-    else
-      render json: {
-        status: 401,
-        logged_in: false
-      }, status: :unauthorized
-    end
-  end
-
-    def logged_in
-      if @current_user
+      if user
+        session[:user_id] = user.id
         render json: {
+          status: :created,
           logged_in: true,
-          user: @current_user
-        }
+          user: user
+        }, status: :created
       else
-        render json: { logged_in: false }
+        render json: {
+          status: 401,
+          logged_in: false
+        }, status: :unauthorized
       end
     end
 
+    def logged_in
+        if @current_user
+          render json: {
+            logged_in: true,
+            user: @current_user
+          }
+        else
+          render json: { logged_in: false }
+        end
+    end
+
     def logout
-      reset_session
-      render json: { status: 200, logged_out: true }
+        reset_session
+        render json: { status: 200, logged_out: true }
     end
 
     def other_users
-      users = User.where.not(id: @current_user&.id)
-      render json: { status: :ok, users: users }
+        users = User.where.not(id: @current_user&.id)
+        render json: { status: :ok, users: users }
     end
 
     private
 
     def session_params
-      params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password)
     end
-end
+  end
 end
