aeb0e7a7c583e5f3f9e013393ed4f61647f688a4
NikEmman <121699424+NikEmman@users.noreply.github.com>
2025-07-15 22:56:05 +0300

    Fix: session store domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env =="production"
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "my-rails-api-app-domain.com"
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "messenger-lr1s.onrender.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
