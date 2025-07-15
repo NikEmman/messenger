f9c0cb3da1920d71ba830fbd4e35d93155317426
NikEmman <121699424+NikEmman@users.noreply.github.com>
2025-07-15 22:36:35 +0300

    Fix: cors.rb issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
-    resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ], credentials: true
+    origins "https://messenger-lr1s.onrender.com"
+
+    resource "*",
+      headers: :any,
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      credentials: true #
   end
+
   allow do
-    # the domain on which the production app will be on
-    origins "https://messenger-lr1s.onrender.com/"
-    resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ], credentials: true
+    origins "http://localhost:3000"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
