624570e323df1193b2c54794f825ca48e2f7937e
NikEmman <121699424+NikEmman@users.noreply.github.com>
2025-09-12 09:30:10 +0300

    Fix: Add route to mount engine that serves files from db

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
+  mount ActiveStorageDB::Engine => "/files"
+
   namespace :api do
     resources :profiles, only: [ :create, :show, :destroy, :update ]
     resources :sessions, only: [ :create ]
     resources :registrations, only: [ :create ]
     delete :logout, to: "sessions#logout"
     get :logged_in, to: "sessions#logged_in"
-
     resources :conversations, only: [ :index, :show, :destroy, :create ]
     resources :conversation_users, only: [ :create ]
     resources :messages, only: [ :create ]
     get "/other_users", to: "sessions#other_users"
   end
+
   root "homepage#index"
   get "up" => "rails/health#show", as: :rails_health_check
-
   get "/messages", to: "homepage#index"
   get "/profile", to: "homepage#index"
   get "/signup", to: "homepage#index"
-  get "*path", to: "homepage#index", constraints: ->(req) { req.format.html? && !req.path.start_with?("/images", "/assets") }
+
+  get "*path", to: "homepage#index", constraints: ->(req) { req.format.html? && !req.path.start_with?("/images", "/assets", "/files") }
 end
