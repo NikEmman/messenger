b8bde301a13b7edfcc79e9d1a9d75bd001378c6a
NikEmman <121699424+NikEmman@users.noreply.github.com>
2026-02-13 13:58:38 +0200

    Refactor CSS to use modular structure in frontend/styles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,12 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./frontend/";
-import "../assets/stylesheets/application.css";
+import "./frontend/styles/App.css";
+import "./frontend/styles/Navbar.css";
+import "./frontend/styles/Conversation.css";
+import "./frontend/styles/GroupChatSide.css";
+import "./frontend/styles/Profile.css";
+import "./frontend/styles/ErrorPage.css";
 
 import "trix";
 import "@rails/actiontext";
